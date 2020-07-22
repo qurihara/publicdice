d327f09b3128724bbc81242eb08ce902444327e8
qurihara <qurihara@unryu.org>
2020-07-22 20:00:31 +0900

    ok

--- randomness_test2/index.js
+++ randomness_test2/index.js
@@ -1,6 +1,44 @@
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-web3.eth.getBlock(0, (error, result) => {
-    console.log(result);
-});
+if (process.argv.length < 3) {
+    console.log('uasge: node./ nonce_eth.js START_BLOCK');
+    process.exit(1);
+}
+const START_BLOCK = +process.argv[2];
+
+function sleep(time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+}
+
+async function main() {
+
+    for (let b = START_BLOCK; b >= 0; b--) {
+        getBlock(b);
+        await sleep(WAIT);
+    }
+}
+main();
+
+function getBlock(block) {
+    web3.eth.getBlock(i, (error, result) => {
+        console.log(result);
+        if (result.hash != undefined) {
+            let rnd = getRand(result.hash, digits);
+            console.log(block + "," + result.hash + "," + rnd + "," + result.nonce);
+        }
+    });
+
+}
+
+const digits = 16; // should determine by max
+function getRand(hash, digits) {
+    let x = hash.substr(hash.length - digits, digits);
+    let d = parseInt(x, 16);
+    let rnd = d / Math.pow(16, digits);
+    return rnd;
+}
